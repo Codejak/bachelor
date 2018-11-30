98b65a7d39f34fa7d776f35b72059b9a96d9157a
Codejak <jakob@sub-link.net>
2018-11-30 14:29:22 +0100

    neuershit

---
+++ sinnvolles/invertingtest.py
@@ -0,0 +1,22 @@
+
+
+from vtk import *
+
+readera = vtk.vtkSTLReader()
+readera.SetFileName(final_shape)
+readera.Update()
+mappera = vtk.vtkPolyDataMapper()
+mappera.SetInputConnection(readera.GetOutputPort())
+actora = vtk.vtkActor()
+actora.SetMapper(mappera)
+rendera = vtk.vtkRenderer()
+rendera.AddActor(actora)
+windowa = vtk.vtkRenderWindow()
+windowa.AddRenderer(rendera)
+windowa.SetSize(200,200)
+interactora = vtk.vtkRenderWindowInteractor()
+interactora.SetRenderWindow(windowa)
+interactora.Initialize()
+windowa.Render()
+interactora.Start()
+
