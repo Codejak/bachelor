94685f4f9adf72db43d9b5afe59f86887ffc8808
Codejak <jakob@sub-link.net>
2018-12-01 18:12:33 +0100

    fortschitt yay

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
-"C:\Program Files\VTK 7.1.1\bin\vtkpython" C:\Users\Jakob\Documents\GitHub\bachelor\sinnvolles\invertingtest.py
+cd C:\Users\Jakob\Documents\GitHub\bachelor\sinnvolles\
+"C:\Program Files\VTK 7.1.1\bin\vtkpython" invertingtest.py
--- sinnvolles/invertingtest.py
+++ sinnvolles/invertingtest.py
@@ -3,22 +3,36 @@
 from vtk import *
 final_shape = "final_shape.stl"
 
+box = vtk.vtkBox()
+
 readera = vtk.vtkSTLReader()
 readera.SetFileName(final_shape)
 readera.Update()
+
+
+"""boolean = vtk.vtkImplicitBoolean()
+boolean.SetOperationTypeToDifference()
+boolean.AddFunction(final_shape)
+boolean.AddFunction(box)
+"""
+
 mappera = vtk.vtkPolyDataMapper()
-mappera.SetInputConnection(readera.GetOutputPort())
+mappera.SetInputConnection(box.GetOutputPort())
+
 actora = vtk.vtkActor()
 actora.SetMapper(mappera)
+
 rendera = vtk.vtkRenderer()
 rendera.AddActor(actora)
+
 windowa = vtk.vtkRenderWindow()
 windowa.AddRenderer(rendera)
 windowa.SetSize(200,200)
+
 interactora = vtk.vtkRenderWindowInteractor()
 interactora.SetRenderWindow(windowa)
 interactora.Initialize()
-windowa.Render()
-interactora.Start()
-print "lol"
 
+windowa.Render()
+interactora.Start() 
+print "lol"
