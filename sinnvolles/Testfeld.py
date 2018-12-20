603f5f288bc344285a3443d09a1c53aa8fa9905f
Codejak <jakob@sub-link.net>
2018-12-20 15:47:27 +0100

    12345

--- sinnvolles/Testfeld.py
+++ sinnvolles/Testfeld.py
@@ -30,7 +30,6 @@ pointsObject1 = []
 pointsObject2 = []
 
 box = vtk.vtkCubeSource()
-#box.SetXLength()
 
 def storePoints(objectToBeStored):
   if objectToBeStored == "polydata1":
@@ -49,7 +48,6 @@ def storePoints(objectToBeStored):
     print "Something went wrong with the point storing..."
 
 
-
 def getBoxValues(objectTobeValued):
   if objectTobeValued == "polydata1":
     xList = []
@@ -62,31 +60,29 @@ def getBoxValues(objectTobeValued):
       yList.append(p[1])
       zList.append(p[2])
 
-    completeList.append(min(xList))
-    completeList.append(max(xList))
-    completeList.append(min(yList))
-    completeList.append(max(yList))
-    completeList.append(min(zList))
-    completeList.append(max(zList))
+    completeList.append(min(xList) + 0.01)
+    completeList.append(max(xList) - 0.01)
+    completeList.append(min(yList) + 0.01)
+    completeList.append(max(yList) - 0.01) 
+    completeList.append(min(zList) + 0.001)
+    completeList.append(max(zList) - 0.001)
   print completeList
   return completeList
 
 
+storePoints("polydata1")
+box.SetBounds(getBoxValues("polydata1"))
 
-
-
-extractGeometryBox = vtk.vtkExtractGeometry()
-extractGeometryBox.SetImplicitFunction(box)
-extractGeometryBox.SetInputData(box)
-
+triangledBox = vtk.vtkTriangleFilter()
+triangledBox.SetInputConnection(box.GetOutputPort())
+triangledBox.Update()
 
 
 booleanfilter = vtk.vtkBooleanOperationPolyDataFilter()
 booleanfilter.SetOperationToDifference()
-booleanfilter.SetInputConnection(0, extractGeometryBox.GetOutputPort())
 booleanfilter.SetInputConnection(1, reader1.GetOutputPort())
-
-
+booleanfilter.SetInputConnection(0, triangledBox.GetOutputPort())
+booleanfilter.SetTolerance(0.000000000000000001)
 
 
 
@@ -96,11 +92,6 @@ smooth1 = vtk.vtkSmoothPolyDataFilter()
 smooth1.SetInputConnection(reader1.GetOutputPort())
 smooth1.Update()
 
-smooth2 = vtk.vtkSmoothPolyDataFilter()
-smooth2.SetInputConnection(reader2.GetOutputPort())
-smooth2.Update()
-
-
 #clean the shit
 clean1 = vtk.vtkCleanPolyData()
 clean1.SetInputConnection(smooth1.GetOutputPort())
@@ -109,27 +100,12 @@ clean1.ConvertPolysToLinesOn()
 clean1.ConvertLinesToPointsOn()
 clean1.Update()
 
-clean2 = vtk.vtkCleanPolyData()
-clean2.SetInputConnection(smooth2.GetOutputPort())
-clean2.ConvertStripsToPolysOn()
-clean2.ConvertPolysToLinesOn()
-clean2.ConvertLinesToPointsOn()
-clean2.Update()
-
-
 #triangle that shit
 triangle1 = vtk.vtkTriangleFilter()
 triangle1.SetInputConnection(clean1.GetOutputPort())
 triangle1.PassVertsOff()
 triangle1.PassLinesOff()
 triangle1.Update()
-
-triangle2 = vtk.vtkTriangleFilter()
-triangle2.SetInputConnection(clean2.GetOutputPort())
-triangle2.PassVertsOff()
-triangle2.PassLinesOff()
-triangle2.Update()
-
 
 #normal that shit
 normal1 = vtk.vtkPolyDataNormals()
@@ -140,15 +116,6 @@ normal1.SplittingOff()
 normal1.ConsistencyOn()
 normal1.AutoOrientNormalsOff()
 normal1.Update()
-
-normal2 = vtk.vtkPolyDataNormals()
-normal2.SetInputConnection(triangle2.GetOutputPort())
-normal2.ComputePointNormalsOn()
-normal2.ComputeCellNormalsOff()
-normal2.SplittingOff()
-normal2.ConsistencyOn()
-normal2.AutoOrientNormalsOff()
-normal2.Update()
 
 booleanfilter = vtk.vtkBooleanOperationPolyDataFilter()
 booleanfilter.SetOperationToDifference()
@@ -193,11 +160,13 @@ actor2.SetMapper(mapper2)
 
 actor3 = vtk.vtkActor()
 actor3.SetMapper(mapper3)
+actor3.GetProperty().EdgeVisibilityOn()
 
 renderer = vtk.vtkRenderer()
 #renderer.AddActor(actor1)
-renderer.AddActor(actor2)
+#renderer.AddActor(actor2)
 renderer.AddActor(actor3)
+renderer.SetBackground(1,1,1)
 
 window = vtk.vtkRenderWindow()
 window.AddRenderer(renderer)
