12e9ea2d0b41034f8a374b687748bd92f9541cb6
Jakob Köntopf <jakob@sub-link.net>
2019-02-08 14:44:24 +0100

    gerettet

--- sinnvolles/Testfeld.py
+++ sinnvolles/Testfeld.py
@@ -11,6 +11,7 @@ gamma = 1
 
 
 
+
 #define the class which creates objects store the data of the STL files
 class STL_Object:
   numberOfSTLObjects = 0
@@ -28,11 +29,16 @@ class STL_Object:
     self.zCoordinates = []
     self.maxX = 0
     self.minX = 0
+    self.xDist = 0
     self.maxY = 0 
     self.minY = 0
+    self.yDist = 0
     self.maxZ = 0
     self.minZ = 0
+    self.zDist = 0
     self.avgMaxDist = 0
+    self.vol = 0
+    self.surfA = 0
 
     #reads the Objekt from the stl file
     self.reader = vtk.vtkSTLReader()
@@ -46,10 +52,12 @@ class STL_Object:
     print "\nTrying to store -%s-! Point Number:%s" %(self.fileName, self.numberOfPoints)
 
     #store the point from the STL file into arrays to work with
+    self.objectPoints = []
     for x in range(self.numberOfPoints):
       self.objectPoints.append(self.polydata.GetPoints().GetPoint(x))
     print "\n%s succesfully stored. Length of the storing array: %d" %(self.fileName, len(self.objectPoints))
 
+    self.updateCoordinates()
 
 
   #determine the (new) number of points in the object
@@ -75,6 +83,10 @@ class STL_Object:
     self.minY = min(self.yCoordinates)
     self.maxZ = max(self.zCoordinates)
     self.minZ = min(self.zCoordinates)
+    self.xDist = self.maxX - self.minX
+    self.yDist = self.maxY - self.minY
+    self.zDist = self.maxZ - self.minZ
+    self.avgMaxDist = (self.xDist+self.yDist+self.zDist)/3
     print "\nCoordinates of %s succesfully updated. Length of the arrays: %s, %s, %s \nThese are the min/max coordinates: X[%s,%s], Y[%s,%s], Z[%s,%s] " %(self.fileName, len(self.xCoordinates), len(self.yCoordinates), len(self.zCoordinates), self.minX,self.maxX,self.minY,self.maxY,self.minZ,self.maxZ)
     self.updateNumberOfPoints()
 
@@ -154,8 +166,18 @@ class STL_Object:
     """
 
 
+  #determine the surface area of the object
+  def getPropertyInfo(self):
+    self.massP = vtk.vtkMassProperties()
+    self.massP.SetInputData(polydata)
+    self.massP.Update()
+    self.vol = self.massP.GetVolume()
+    self.surfA = self.massP.GetSurfaceArea()
+
+
   #vizualize the object 
   def visualize(self):
+
     self.mapper = vtk.vtkPolyDataMapper()
     self.mapper.SetInputData(self.polydata)
     #self.mapper.SetInputConnection(self.booleanfilter.GetOutputPort())
@@ -163,6 +185,7 @@ class STL_Object:
     self.actor = vtk.vtkActor()
     self.actor.SetMapper(self.mapper)
     self.actor.GetProperty().EdgeVisibilityOn()
+
 
 
 #determine a value based on the comparison of 2 object
@@ -178,6 +201,8 @@ def determineHD(origObject, randObject):
     pointDistList = []
     for y in origObject.objectPoints:
       pointDistList.append(sqrt(vtkMath.Distance2BetweenPoints(x,y)))
+    distListHD.append(min(pointDistList))
+  print distListHD
 
   for a in distListHD:
     addedDistHD += a
@@ -189,11 +214,9 @@ def determineHD(origObject, randObject):
 
   distHD = max(distListHD)
   finalValueHD = (1/(1+aplha*stdDevHD)) * (1/(1+beta*(avgDistHD/randObject.avgMaxDist))) * (1/(1+gamma*(distHD/randObject.avgMaxDist)))
-
+  
   print  len(distListHD), finalValueHD
   return finalValueHD
-
-
 
 
 
@@ -207,22 +230,9 @@ originalObject.buildNegative()
 generatedObject.buildNegative()
 originalObject.visualize()
 generatedObject.visualize()
+#print determineHD(originalObject, generatedObject)
 
 
-
-
-"""
-mass1 = vtk.vtkMassProperties()
-mass1.SetInputConnection(reader1.GetOutputPort())
-mass1.Update()
-volume1 = mass1.GetVolume()
-
-mass2 = vtk.vtkMassProperties()
-mass2.SetInputConnection(reader2.GetOutputPort())
-mass2.Update()
-volume2 = mass2.GetVolume()
-
-"""
 
 
 renderer = vtk.vtkRenderer()
@@ -259,6 +269,8 @@ widget2.SetInteractor(interactor2)
 widget2.SetEnabled(1)
 widget2.InteractiveOn()
 
+
+
 window.Render()
 window2.Render()
 interactor.Initialize()
@@ -269,3 +281,7 @@ interactor2.Start()
 
 
 
+
+"""
+print distListHD
+"""
